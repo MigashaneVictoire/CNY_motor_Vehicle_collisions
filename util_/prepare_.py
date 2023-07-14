c7843e0235ac6ff805115d9bed7b0aa9a5ac2b9a
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-07-14 15:09:22 -0500

    Add save visuals function and modeling file

--- util_/prepare_.py
+++ util_/prepare_.py
@@ -1,20 +1,37 @@
-# For funtion annotations
+##########################################################################################
+# Imports
+##########################################################################################
+# funtion annotations
 from binascii import a2b_qp
 from typing import Union
 from typing import Tuple
 
-# Python libraries
-import numpy as np
+# data manipulation
 import pandas as pd
+import numpy as np
+
+# data visualization
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# data separation/transformation
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+# system manipulation
 import os
-
-# Personal libraries
+import sys
+sys.path.append("./util_")
 import acquire_
-# import env
 
+# other
+import env
+import warnings
+warnings.filterwarnings("ignore")
+
+##########################################################################################
+# Project specific functions
+##########################################################################################
 def drop_cols(df: pd.DataFrame, cols: list) -> pd.DataFrame:
     """
     Goal: drop redundent columns from the data
@@ -33,7 +50,6 @@ def drop_cols(df: pd.DataFrame, cols: list) -> pd.DataFrame:
     
     return df
 
-# -----------------------------------------------------------------
 # get data without dummies
 def get_vehicle_data_no_dummies() -> pd.DataFrame:
     """
@@ -245,7 +261,6 @@ def get_vehicle_data_no_dummies() -> pd.DataFrame:
 
     return vehicle
 
-# -----------------------------------------------------------------
 # get data with dummies
 def get_vehicle_data_with_dummies() -> pd.DataFrame:
     """
@@ -491,10 +506,74 @@ def get_vehicle_data_with_dummies() -> pd.DataFrame:
 
     return train
 
+##########################################################################################
+# Save visuals
+##########################################################################################
+def save_visuals_(fig: plt.figure ,viz_name:str= "unamed_viz", folder_name:int= 0, ) -> str:
+    """
+    Goal: Save a single visual into the project visual folder
+    parameters:
+        fig: seaborn visual figure to be saved
+        viz_name: name of the visual to save
+        folder_name: interger (0-7)represanting the section you are on in the pipeline
+            0: all other (defealt)
+            1: univariate stats
+            2: bivariate stats
+            3: multivariate stats
+            4: stats test
+            5: modeling
+            6: final report
+            7: presantation
+    return:
+        message to user on save status
+    """
+    project_visuals = "./00_project_visuals"
+    folder_selection = {
+        0: "00_non_specific_viz",
+        1: "01_univariate_stats_viz",
+        2: "02_bivariate_stats_viz",
+        3: "03_multivariate_stats_viz",
+        4: "04_stats_test_viz",
+        5: "05_modeling_viz",
+        6: "06_final_report_viz",
+        7: "07_presantation"
+    }
 
-# -----------------------------------------------------------------
+    # return error if user input for folder selection is not found
+    if folder_name not in list(folder_selection.keys()):
+        return f"{folder_name} is not a valid option for a folder name."
+    # when folder location is found in selections
+    else:
+        # Specify the path to the directory where you want to save the figure
+        folder_name = folder_selection[folder_name]
+        directory_path = f'{project_visuals}/{folder_name}/'
+
+        # Create the full file path by combining the directory path and the desired file name
+        file_path = os.path.join(directory_path, f'{viz_name}.png')
+
+        if os.path.exists(project_visuals): # check if the main viz folder exists
+            if not os.path.exists(directory_path): # check if the folder name already exists
+                os.makedirs(directory_path)
+                # Save the figure to the specified file path
+                fig.canvas.print_figure(file_path)
+
+            else:
+                # Save the figure to the specified file path
+                fig.canvas.print_figure(file_path)
+        else:
+            # create both the project vis folder and the specific section folder
+            os.makedirs(project_visuals)
+            os.makedirs(directory_path)
+
+            # Save the figure to the specified file path
+            fig.canvas.print_figure(file_path)
+    
+    return f"Visual successfully saved in folder: {folder_name}"
+
+##########################################################################################
 # Save the splited data into separate csv files
-def save_split_data(encoded_df: pd.DataFrame, train:pd.DataFrame, validate:pd.DataFrame, test:pd.DataFrame, folder_path: str = "./project_data") -> str:
+##########################################################################################
+def save_split_data_(encoded_df: pd.DataFrame, train:pd.DataFrame, validate:pd.DataFrame, test:pd.DataFrame, folder_path: str = "./project_data") -> str:
     """
     parameters:
         encoded_df: full project dataframe that contains the (encoded columns or scalling)
@@ -535,10 +614,9 @@ def save_split_data(encoded_df: pd.DataFrame, train:pd.DataFrame, validate:pd.Da
 
     return "Four data sets saved as .csv"
 
-
-
-# -----------------------------------------------------------------
+##########################################################################################
 # Split the data into train, validate and train
+##########################################################################################
 def split_data_(df: pd.DataFrame, test_size: float =.2, validate_size: float =.2, stratify_col: str =None, random_state: int=95) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     '''
     parameters:
